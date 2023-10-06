e99fc8f8776275fb744e2094636b3db4b3390c89
kwazaar <maksim_sizov@icloud.com>
2023-10-06 07:56:02 +0300

    Добавил определение месторасположения

--- WalkAndRunTest/ViewControllers/NavigationViewController.swift
+++ WalkAndRunTest/ViewControllers/NavigationViewController.swift
@@ -16,32 +16,23 @@ class NavigationViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
     }
     override func viewDidAppear(_ animated: Bool) {
         checkLocationEnabled()
+        checkAutorization()
     }
     
     func checkLocationEnabled() {
-//        DispatchQueue.global().async {
-//            if CLLocationManager.locationServicesEnabled() {
-//                self.setupManager()
-//            } else {
-//                let alert = UIAlertController(title: "У вас выключена служба геолокации", message: "Включите службу для корректного отображения", preferredStyle: .alert)
-//                let settingAction = UIAlertAction(title: "Настройки", style: .default) { (alert) in
-//                    if let url = URL(string: "App-Prefs:root=LOCATION_SERVICES") {
-//                        UIApplication.shared.open(url)
-//                    }
-//                    
-//                }
-//                let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
-//                
-//                alert.addAction(settingAction)
-//                alert.addAction(cancelAction)
-//                
-//                self.present(alert, animated: true, completion: nil)
-//            }
-//        }
+        DispatchQueue.global().async {
+            if CLLocationManager.locationServicesEnabled() {
+                self.setupManager()
+                
+            } else {
+                self.showAlertMassage(title: "Перейдите в настойки и включите геолокацию", massage: "Перейти", url: URL(string: "App-Prefs:root=LOCATION_SERVICES"))
+            }
+        }
         
     }
     
@@ -50,6 +41,52 @@ class NavigationViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         
+    }
+    
+    func checkAutorization() {
+        
+        switch locationManager.authorizationStatus {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .denied:
+                showAlertMassage(title: "Вы запретили использовать геолокацию", massage: "Хотите включить?", url: URL(string: UIApplication.openSettingsURLString))
+        case .authorizedAlways:
+            break
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            print("Неизвестная ошибка")
+        }
+    }
+    
+    func showAlertMassage(title: String, massage: String?, url: URL?) {
+
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let settingAction = UIAlertAction(title: "Настройки", style: .default) { (alert) in
+            if let url = url {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+            
+        }
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
+        
+        alert.addAction(settingAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last?.coordinate {
+            let region = MKCoordinateRegion(center: location, latitudinalMeters: 5000, longitudinalMeters: 5000)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkAutorization()
     }
 
 }
