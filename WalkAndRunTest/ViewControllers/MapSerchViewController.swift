1c6b8bb55a6ef2e5b3ad5be198609e29258062b4
kwazaar <maksim_sizov@icloud.com>
2023-10-11 07:15:16 +0300

    Добавил serchBar и реализовал поиск мест на карте

--- WalkAndRunTest/ViewControllers/MapSerchViewController.swift
+++ WalkAndRunTest/ViewControllers/MapSerchViewController.swift
@@ -8,17 +8,25 @@
 import UIKit
 import MapKit
 
-class MapSerchViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class MapSerchViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UISearchBarDelegate, UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        print("Update")
+    }
+    
+    
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var serchButton: UIButton!
     
     let locationManager = CLLocationManager()
+    let searchController = UISearchController(searchResultsController: nil)
     var destinationCoordinate: CLLocationCoordinate2D?
     var anotationsArray = [MKPointAnnotation]()
+    var userLocationSet = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         serchButton.isHidden = true
         let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(gesture:)))
         mapView.addGestureRecognizer(longPressGesture)
@@ -28,7 +36,48 @@ class MapSerchViewController: UIViewController, MKMapViewDelegate, CLLocationMan
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         
+        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Поиск места"
+        searchController.searchBar.delegate = self
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
     }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        performSearch()
+    }
+    func performSearch() {
+        
+         guard let searchText = searchController.searchBar.text, !searchText.isEmpty else {
+             return
+         }
+         
+         let request = MKLocalSearch.Request()
+         request.naturalLanguageQuery = searchText
+         
+         let search = MKLocalSearch(request: request)
+         search.start { (response, error) in
+             guard let response = response else {
+                 if let error = error {
+                     print("Error occurred in search: \(error.localizedDescription)")
+                 }
+                 return
+             }
+             
+             // Удалить предыдущие аннотации на карте
+             self.mapView.removeAnnotations(self.mapView.annotations)
+             
+             // Добавить новые аннотации на карту на основе результатов поиска
+             for item in response.mapItems {
+                 let annotation = MKPointAnnotation()
+                 annotation.coordinate = item.placemark.coordinate
+                 annotation.title = item.name
+                 self.mapView.addAnnotation(annotation)
+             }
+         }
+     }
     
     @IBAction func showRoute(_ sender: UIButton) {
         drawRouteToMarker()
@@ -40,8 +89,7 @@ class MapSerchViewController: UIViewController, MKMapViewDelegate, CLLocationMan
             let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
             addMarkerToMapView(coordinate: coordinate)
             
-            serchButton.isHidden = false
-            destinationCoordinate = coordinate
+            
         }
     }
     
@@ -53,15 +101,18 @@ class MapSerchViewController: UIViewController, MKMapViewDelegate, CLLocationMan
         annotation.coordinate = coordinate
         mapView.addAnnotation(annotation)
         anotationsArray.append(annotation)
+        mapView.selectAnnotation(annotation, animated: true)
         
     }
+    
     func drawRouteToMarker() {
-        
-        
+        self.mapView.removeAnnotations(self.mapView.annotations)
         guard let destinationCoordinate = destinationCoordinate else { return }
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = destinationCoordinate
+        mapView.addAnnotation(annotation)
         
         let endPoint = MKPlacemark(coordinate: destinationCoordinate)
-        
         let request = MKDirections.Request()
         request.source = MKMapItem.forCurrentLocation()
         request.destination = MKMapItem(placemark: endPoint)
@@ -72,6 +123,12 @@ class MapSerchViewController: UIViewController, MKMapViewDelegate, CLLocationMan
             guard let response = response else { return }
             
             for route in response.routes {
+                if !self.mapView.overlays.isEmpty {
+                    self.mapView.removeOverlay(self.mapView.overlays.first!)
+                }
+                if self.anotationsArray.count > 1 {
+                    self.mapView.removeAnnotation(self.anotationsArray.removeFirst())
+                }
                 self.mapView.addOverlay(route.polyline)
             }
         }
@@ -95,18 +152,25 @@ class MapSerchViewController: UIViewController, MKMapViewDelegate, CLLocationMan
         }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.last?.coordinate {
-            let region = MKCoordinateRegion(center: location, latitudinalMeters: 5000, longitudinalMeters: 5000)
-            mapView.setRegion(region, animated: true)
+        if !userLocationSet {
+            if let location = locationManager.location?.coordinate {
+                 let region = MKCoordinateRegion(center: location, latitudinalMeters: 2000, longitudinalMeters: 2000)
+                mapView.setRegion(region, animated: true)
+            }
+            userLocationSet = true
         }
     }
-    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        
-    }
+
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let render = MKPolylineRenderer(overlay: overlay)
         render.strokeColor = .orange
         render.lineWidth = 4
         return render
+    }
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        if let selectedMarker = view.annotation as? MKPointAnnotation {
+            destinationCoordinate = selectedMarker.coordinate
+            serchButton.isHidden = false
+        }
     }
 }
