57eaefb88b538850292f076ba12c070e0d2f0625
kwazaar <maksim_sizov@icloud.com>
2023-11-08 07:17:46 +0300

    Создал ScrollView с тестовым массивом для подготовке к реализации ленты новостей

---
+++ WalkAndRunTest/ViewControllers/Cell/NewsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsCollectionViewCell.swift
+//  WalkAndRunTest
+//
+//  Created by MaxOS on 08.11.2023.
+//
+
+import UIKit
+
+class NewsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lableCell: UILabel!
+    
+}
--- WalkAndRunTest/ViewControllers/MainViewController.swift
+++ WalkAndRunTest/ViewControllers/MainViewController.swift
@@ -9,24 +9,35 @@ import UIKit
 import CoreLocation
 
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var tempWeather: UILabel!
     @IBOutlet weak var cityWeather: UILabel!
     @IBOutlet weak var conditionsWeather: UILabel!
     @IBOutlet weak var imageWeather: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var collectionViewHeightConstraint: NSLayoutConstraint!
     
     var weatherService = WeatherAPI()
     var location: Step = Step()
     var weatherResponse: WeatherResponse?
     var weatherData: WeatherData?
     var locationManager = CLLocationManager()
+    
+    var testArray = ["1", "2", "3", "4", "5"]
+
 
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        
         navigationItem.hidesBackButton = true
         guard let currentLocation = locationManager.location?.coordinate else { return }
         location.latitude = currentLocation.latitude
@@ -46,7 +57,6 @@ class MainViewController: UIViewController {
                         self.imageWeather.image = UIImage(named: String(decodeData.current.condition.code))
                         
                         self.weatherData = decodeData
-//                        print("Количество дней \(decodeData.forecast.forecastday.count)")
                         
                     }
 
@@ -58,8 +68,7 @@ class MainViewController: UIViewController {
                 }
             
         }
-
-}
+    }
     
     @IBAction func showProfile(_ sender: UIButton) {
         
@@ -85,4 +94,27 @@ class MainViewController: UIViewController {
         vc.weatherData = weatherData
         self.navigationController?.pushViewController(vc, animated: true)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewsCell", for: indexPath) as! NewsCollectionViewCell
+        cell.lableCell.text = testArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return testArray.count
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let yOffset = collectionView.contentOffset.y
+        let contentSize = collectionView.contentSize
+        
+        if collectionViewHeightConstraint.constant < 700 {
+            collectionViewHeightConstraint.constant = 278 + yOffset
+        }
+        
+    }
+    
 }
